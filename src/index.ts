449939248222f28a32b3a161e1b0f82291b832f0
daiki510 <ddgg7755@gmail.com>
2023-04-13 00:18:54 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('Hello World!!');
+import { App, KnownEventFromType } from '@slack/bolt';
+import { load } from 'ts-dotenv';
+
+const env = load({
+  SLACK_BOT_TOKEN: String,
+  SLACK_SIGNING_SECRET: String,
+  PORT: Number,
+});
+
+// SlackBotの初期化
+const app = new App({
+  token: env.SLACK_BOT_TOKEN,
+  signingSecret: env.SLACK_SIGNING_SECRET,
+});
+
+// "hello" を含むメッセージをリッスンします
+app.message('', async ({ message, say }) => {
+  // イベントがトリガーされたチャンネルに say() でメッセージを送信します
+  if (
+    message.subtype !== 'message_deleted' &&
+    message.subtype !== 'message_replied' &&
+    message.subtype !== 'message_changed'
+  ) {
+    await say(`Hello, <@${message.text}>`);
+  }
+});
+
+(async () => {
+  // アプリを起動します
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
