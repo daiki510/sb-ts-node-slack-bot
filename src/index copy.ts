c5dbf1d04b8a6cd61d684602b480da189768ba65
daiki510 <ddgg7755@gmail.com>
2023-04-20 21:44:03 +0900

    update

---
+++ src/index copy.ts
@@ -0,0 +1,63 @@
+import { App, KnownEventFromType } from '@slack/bolt';
+import { load } from 'ts-dotenv';
+
+const env = load({
+  SLACK_BOT_TOKEN: String,
+  SLACK_SIGNING_SECRET: String,
+  PORT: Number,
+});
+
+// SlackBotの初期化
+const app = new App({
+  token: env.SLACK_BOT_TOKEN,
+  signingSecret: env.SLACK_SIGNING_SECRET,
+});
+
+// app.event('message', async ({ event, client, logger }) => {
+//   try {
+//     const result = await client.chat.postMessage({
+//       channel: event.channel,
+//       text: `Hello world!`,
+//     });
+//     logger.info(result);
+//   } catch (error) {
+//     logger.error(error);
+//   }
+// });
+
+app.message('', async ({ message, say }) => {
+  if (!message.subtype) {
+    // await say({
+    //   blocks: [
+    //     {
+    //       type: 'section',
+    //       text: {
+    //         type: 'mrkdwn',
+    //         text: `Hey there <@${message.user}>!`,
+    //       },
+    //       accessory: {
+    //         type: 'button',
+    //         text: {
+    //           type: 'plain_text',
+    //           text: 'Click Me',
+    //         },
+    //         action_id: 'button_click',
+    //       },
+    //     },
+    //   ],
+    //   text: `Hey there <@${message.user}>!`,
+    // });
+    await say(`Hello, <@${message.user}>. You said: ${message.text}`);
+  }
+});
+
+// app.action('button_click', async ({ body, ack, say }) => {
+//   await ack();
+//   await say(`<@${body.user.id}> clicked the button`);
+// });
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,28 @@
-import { App, KnownEventFromType } from '@slack/bolt';
+import { App } from '@slack/bolt';
 import { load } from 'ts-dotenv';
 
+//②SlackBotのトークンとシークレットを環境変数から読み込む
 const env = load({
   SLACK_BOT_TOKEN: String,
   SLACK_SIGNING_SECRET: String,
   PORT: Number,
 });
 
-// SlackBotの初期化
+//③SlackBotの初期化
 const app = new App({
   token: env.SLACK_BOT_TOKEN,
   signingSecret: env.SLACK_SIGNING_SECRET,
 });
 
-// app.event('message', async ({ event, client, logger }) => {
-//   try {
-//     const result = await client.chat.postMessage({
-//       channel: event.channel,
-//       text: `Hello world!`,
-//     });
-//     logger.info(result);
-//   } catch (error) {
-//     logger.error(error);
-//   }
-// });
-
+//④SlackBotにメッセージを送信する
 app.message('', async ({ message, say }) => {
   if (!message.subtype) {
-    await say({
-      blocks: [
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: `Hey there <@${message.user}>!`,
-          },
-          accessory: {
-            type: 'button',
-            text: {
-              type: 'plain_text',
-              text: 'Click Me',
-            },
-            action_id: 'button_click',
-          },
-        },
-      ],
-      text: `Hey there <@${message.user}>!`,
-    });
-    // await say(`Hello, <@${message.user}>. You said: ${message.text}`);
+    await say(`Hello, <@${message.user}>. You said: ${message.text}`);
   }
 });
 
-app.action('button_click', async ({ body, ack, say }) => {
-  await ack();
-  await say(`<@${body.user.id}> clicked the button`);
-});
-
 (async () => {
+  //⑤SlackBotを起動する
   await app.start(process.env.PORT || 3000);
 
   console.log('⚡️ Bolt app is running!');
